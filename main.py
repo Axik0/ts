1cbda364d8fa898efd769e41dc2918a39db4938f
Axik0 <xla.2@yandex.ru>
2022-05-13 23:03:38 +0300

    text_field part works properly

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import string
+
+# prepare character lists for future key bindings
+LETTERS = list(string.ascii_lowercase)
+DIGITS = list(string.digits)
+# wow, this list automation takes care of  problematic cases (includes both "'" and '"')
+MARKS = list(string.punctuation)
+EXTRAS = ['<space>', '<Return>']
+
+EXAMPLE = 'Pineapple Apple Pen'
+CHARSET = LETTERS+EXTRAS
 
 # icon import&resize (non-crappy thanks to this pillow fork)
 icon = Image.open("images/monkey.png")
@@ -14,8 +25,6 @@ text_color_ina = '#848484'
 # Global font
 main_font = ("Helvetica", 16)
 
-EXAMPLE = 'wwwwww3422341d'
-
 # GUI preliminaries
 window = Tk()
 window.configure(background=main_color)
@@ -26,30 +35,73 @@ window.title(" Typing monke")
 icon_tk = ImageTk.PhotoImage(icon)
 window.iconphoto(False, icon_tk)
 
+
+def binder(command, lst=CHARSET, mode=True):
+    """takes key names from the list and binds/unbinds to/from a single callback"""
+    for key in lst:
+        text_field.bind(key, command) if mode else text_field.unbind(key)
+
+# k_binder(test, ['a','w'], EXTRAS)
+
+
+
 main_label = Label(window, text="something", font=main_font)
 main_label.pack()
 
+# butt = Button(text='test', width=15, height=3, command=callback)
+# butt.pack()
+
 text_field = Text(window, bg=focus_color, fg=text_color_a, height=100, width=100, font=main_font, relief='sunken')
-text_field.insert("1.0", EXAMPLE)
+
 text_field.pack()
 text_field.focus_set()
-text_field.mark_set(INSERT, "insert-1c")
-print(text_field.index(INSERT))
-text_field.delete("insert-1c", INSERT)
-print(text_field.get("1.0", "end-1c"))
 
-# for all char in length_EXAMPLE
-# set cursor / counter to 1,0
-c_ind = 0
-# wait for input
-# get last character, compare with next
-# if same then change text color, move cursor
-# if True:
-#     text_field.config(fg=text_color_ina)
-#     c_ind += 1
-# else delete this character
+# window.bind('d', check)
+
+# print(text_field.index(INSERT))
+# text_field.delete("insert-1c", "insert")
+# print(text_field.get("1.0", "end-1c"))
 
 
+def text_handler(text: str):
+    # insert the given text into the text field
+    text_field.insert("1.0", text)
+    # set a cursor (=insert mark) to starting position
+    text_field.mark_set("insert", "1.0")
+    # what if we don't allow typing but move a cursor and color current progress?
+    text_field['state'] = 'disabled'
+
+    # prepare a variable to check for success
+    curr_succ = BooleanVar()
+    curr_succ.set(False)
+
+    # get ready to catch (and compare on the fly) inputs from a keyboard
+    def check(key_pressed):
+        """compares an event (some symbol has just been typed) with a next symbol (on the right) of the text line"""
+        if key_pressed.char == text_field.get("insert", "insert+1c").lower():
+            curr_succ.set(True)
+            print(key_pressed.char)
+        # delete this character anyway;
+        # text_field.delete("insert-1c", "insert")
+    binder(check, CHARSET)
+    for _ in text:
+        # stop, wait until curr_succ variable changes (False->True) i.e. until we will have got a correct key
+        window.wait_variable(curr_succ)
+        curr_succ.set(False)
+        # color everything up to and including current character to mark as processed (aka INActive)
+        text_field.tag_add("prev", "1.0", "insert+1c")
+        text_field.tag_config("prev", foreground=text_color_ina)
+        # move cursor to the next character
+        text_field.mark_set("insert", "insert+1c")
+    # prevent user from typing any further, 2 lines could be deleted with a SAME gain but this way is more stable
+    binder(check, mode=False)
+    # after all, get rid of old tag and change the text color back to normal
+    text_field.tag_delete("prev")
+    text_field['fg'] = text_color_a
+    # text_handler(text)
+
+
+text_handler(EXAMPLE)
 
 window.mainloop()
 
