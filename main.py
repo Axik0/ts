c4777878a819ce9b38db1e6e63d8afcc87c6e43a
Axik0 <xla.2@yandex.ru>
2022-05-14 15:51:14 +0300

    +punctuation and char case handlers +new container with checkmarks for those settings sets for characters instead of lists

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ import string
 import time
 
 # prepare character lists for future key bindings
-LETTERS = list(string.ascii_lowercase)+list(string.ascii_uppercase)
-DIGITS = list(string.digits)
+LETTERS_L = set(string.ascii_lowercase)
+LETTERS_U = set(string.ascii_uppercase)
+DIGITS = set(string.digits)
 # wow, this list automation takes even care of the problematic cases (includes both "'" and '"')
-MARKS = list(string.punctuation)
+MARKS = set(string.punctuation)-{"<"}
+print(MARKS)
 EXTRAS = ['<space>']
 
-EXAMPLE = 'Pineapple Apple Pen'
-CHARSET = LETTERS+EXTRAS
+EXAMPLE = 'Pineapple, Apple Pen'
 TOP_CPS = 10
 
-# image import&resize (non-crappy thanks to this pillow fork)
+charset = set.union(LETTERS_L, LETTERS_U, EXTRAS, MARKS)
+markset = set.union(MARKS, {" "})
+
+# image import&resize (non-crappy now thanks to this pillow fork)
 icon = Image.open("images/monkey.png")
 icon = icon.resize((35, 35), Image.Resampling.LANCZOS)
 rfr = Image.open("images/refresh.png")
@@ -34,15 +38,15 @@ main_font = ("Helvetica", 16)
 # GUI preliminaries
 window = Tk()
 window.configure(background=main_color)
-window.geometry("520x200")
-window.minsize(520, 200)
+window.geometry("520x225")
+window.minsize(520, 225)
 window.maxsize(600, 300)
 window.title("Typing Monke")
 icon_tk = ImageTk.PhotoImage(icon)
 window.iconphoto(False, icon_tk)
 
 
-def binder(command, lst=CHARSET, mode=True):
+def binder(command, lst=charset, mode=True):
     """takes key names from the list and binds/unbinds to/from a single callback"""
     for key in lst:
         text_field.bind(key, command) if mode else text_field.unbind(key)
@@ -57,12 +61,17 @@ def text_handler(text: str):
     # what if we don't allow typing but move (hidden) cursor and just color the current progress?
     text_field['state'] = 'disabled'
 
+    # get settings from checkbuttons
+    settings = s1.get(), s2.get()
+
     # drop tempo variables to defaults
     window.scv.set(0)
     c_time, c_counter = 0, 0
 
     # prepare a variable to check for success and count errors
     curr_succ = BooleanVar()
+    # has to change in order to skip possibly unfinished game
+    curr_succ.set(True)
     curr_succ.set(False)
     curr_error = IntVar()
     curr_error.set(0)
@@ -70,18 +79,30 @@ def text_handler(text: str):
     # get ready to catch (and compare on the fly) inputs from a keyboard
     def check(key_pressed):
         """compares an event (some symbol has just been typed) with a next symbol (on the right) of the text line"""
-        if key_pressed.char.lower() == text_field.get("insert", "insert+1c").lower():
-            curr_succ.set(True)
+        if key_pressed.char.lower() == next_char.lower() or key_pressed.char.upper() == next_char.upper():
+            # if our characters differ only by their cases, compare precisely or skip if the setting allows
+            if key_pressed.char == next_char or not settings[1]:
+                curr_succ.set(True)
+        elif settings[0] and prev_char in markset and key_pressed.char == prev_char:
+            # what if we accidentally obey punctuation when we don't need it? Skip to next, not an error!
+            pass
         else:
             print(key_pressed.char)
             curr_error.set(curr_error.get()+1)
-    binder(check, CHARSET)
+    binder(check, charset)
     retry_butt['image'] = tp_tk
     for _ in text:
-        # stop, wait until curr_succ variable changes (False->True) i.e. until we will have got a correct key
-        tic = time.perf_counter()
-        window.wait_variable(curr_succ)
-        toc = time.perf_counter()
+        next_char = text_field.get("insert", "insert+1c")
+        prev_char = text_field.get("insert-1c", "insert")
+        # this step takes care of punctuation according to our settings
+        if next_char in markset and settings[0]:
+            # as we skip this step, it shouldn't count
+            c_counter -= 1
+        else:
+            # stop, wait until curr_succ variable changes (False->True) i.e. until we will have got a correct key
+            tic = time.perf_counter()
+            window.wait_variable(curr_succ)
+            toc = time.perf_counter()
         curr_succ.set(False)
         # color everything up to and including current character to mark as processed (aka INActive)
         text_field.tag_add("prev", "1.0", "insert+1c")
@@ -119,17 +140,27 @@ text_field.grid(row=1, column=0, columnspan=2, padx=18)
 
 window.scv = IntVar()
 w2 = Scale(window, from_=0, to=100, tickinterval=25, orient=HORIZONTAL, length=400, state=DISABLED, showvalue=False,
-           font=main_font, label=f'CPS Rate (relative to {TOP_CPS})', variable=window.scv, troughcolor=focus_color)
+           font=main_font, label=f'CPS Rate (%, relative to {TOP_CPS})', variable=window.scv, troughcolor=focus_color)
 w2.grid(row=2, column=0, sticky=N+S+W+E, padx=15)
 
 rfr_tk = ImageTk.PhotoImage(rfr)
 tp_tk = ImageTk.PhotoImage(tp)
 retry_butt = Button(text='Retry', image=rfr_tk, command=lambda: text_handler(EXAMPLE))
-retry_butt.grid(row=2, column=1, sticky=N+S+W, ipadx=5, pady=10)
+retry_butt.grid(row=2, column=1, sticky=N+S+W, ipadx=5, pady=10, rowspan=2)
+
+
+setup_container = Frame(window)
+setup_container.grid(row=3, column=0)
+s1 = BooleanVar()
+set1 = Checkbutton(setup_container, text="Auto punctuation", selectcolor=focus_color, variable=s1
+                   , onvalue=True, offvalue=False)
+set1.grid(row=0, column=0)
+s2 = BooleanVar()
+set2 = Checkbutton(setup_container, text="Case sensitive", selectcolor=focus_color, variable=s2
+                   , onvalue=True, offvalue=False)
+set2.grid(row=0, column=1)
 
 if __name__ == "__main__":
     text_handler(EXAMPLE)
 
 window.mainloop()
-
-# TODO toggle case sensitivity via button/slider
