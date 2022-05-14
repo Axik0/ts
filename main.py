ef55e90fc2016cca5c995aa48c0bdf6c538e6d19
Axik0 <xla.2@yandex.ru>
2022-05-14 16:50:46 +0300

    extras

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ LETTERS_U = set(string.ascii_uppercase)
 DIGITS = set(string.digits)
 # wow, this list automation takes even care of the problematic cases (includes both "'" and '"')
 MARKS = set(string.punctuation)-{"<"}
-print(MARKS)
 EXTRAS = ['<space>']
 
 EXAMPLE = 'Pineapple, Apple Pen'
@@ -17,6 +16,8 @@ TOP_CPS = 10
 
 charset = set.union(LETTERS_L, LETTERS_U, EXTRAS, MARKS)
 markset = set.union(MARKS, {" "})
+
+em = False
 
 # image import&resize (non-crappy now thanks to this pillow fork)
 icon = Image.open("images/monkey.png")
@@ -68,6 +69,11 @@ def text_handler(text: str):
     window.scv.set(0)
     c_time, c_counter = 0, 0
 
+    # after/before all, get rid of old tag and change the text color back to normal
+    # luckily it works even if "prev" doesn't exist yet so I can leave it here
+    text_field.tag_delete("prev")
+    text_field['fg'] = text_color_a
+
     # prepare a variable to check for success and count errors
     curr_succ = BooleanVar()
     # has to change in order to skip possibly unfinished game
@@ -118,45 +124,47 @@ def text_handler(text: str):
             break
     # prevent user from typing any further, 2 lines could be deleted with a SAME gain but this way is more stable
     binder(check, mode=False)
-    # after all, get rid of old tag and change the text color back to normal
-    text_field.tag_delete("prev")
-    text_field['fg'] = text_color_a
     # tempo calculations (with errors)
     average_cps = (len(text) - 1 - curr_error.get())/c_time
     relative_cps = 100*average_cps/TOP_CPS
     window.scv.set(round(relative_cps))
     if relative_cps >= 90:
         retry_butt['image'] = icon_tk
+        global em
+        em = True
     else:
         retry_butt['image'] = rfr_tk
 
 
-head_label = Label(window, text="Start typing the line below:", font=main_font)
+head_label = Label(window, text="Start typing the line below:", font=main_font, bg=main_color)
 head_label.grid(row=0, column=0, columnspan=2, pady=5)
 
-text_field = Text(window, bg=focus_color, fg=text_color_a, height=3, width=40, font=main_font, relief='sunken')
+text_field = Text(window, bg=focus_color, fg=text_color_a, height=3, width=40, font=main_font, relief=SUNKEN)
 text_field.focus_set()
 text_field.grid(row=1, column=0, columnspan=2, padx=18)
 
 window.scv = IntVar()
 w2 = Scale(window, from_=0, to=100, tickinterval=25, orient=HORIZONTAL, length=400, state=DISABLED, showvalue=False,
-           font=main_font, label=f'CPS Rate (%, relative to {TOP_CPS})', variable=window.scv, troughcolor=focus_color)
+           font=main_font, label=f'CPS Rate (%, relative to {TOP_CPS})', variable=window.scv,
+           troughcolor=focus_color, highlightbackground=main_color, bg=main_color)
 w2.grid(row=2, column=0, sticky=N+S+W+E, padx=15)
 
 rfr_tk = ImageTk.PhotoImage(rfr)
 tp_tk = ImageTk.PhotoImage(tp)
-retry_butt = Button(text='Retry', image=rfr_tk, command=lambda: text_handler(EXAMPLE))
+# et = "You woke up one day with a dream... Harem full of demon girls."
+retry_butt = Button(text='Retry', image=rfr_tk, command=lambda: text_handler(EXAMPLE), bg=main_color)
 retry_butt.grid(row=2, column=1, sticky=N+S+W, ipadx=5, pady=10, rowspan=2)
 
 
 setup_container = Frame(window)
 setup_container.grid(row=3, column=0)
 s1 = BooleanVar()
-set1 = Checkbutton(setup_container, text="Auto punctuation", selectcolor=focus_color, variable=s1
-                   , onvalue=True, offvalue=False)
+s1.set(True)
+set1 = Checkbutton(setup_container, text="Auto punctuation", selectcolor=focus_color, background=main_color, variable=s1
+                   , onvalue=True, offvalue=False, bg=main_color)
 set1.grid(row=0, column=0)
 s2 = BooleanVar()
-set2 = Checkbutton(setup_container, text="Case sensitive", selectcolor=focus_color, variable=s2
+set2 = Checkbutton(setup_container, text="Case sensitive", selectcolor=focus_color, background=main_color, variable=s2
                    , onvalue=True, offvalue=False)
 set2.grid(row=0, column=1)
 
